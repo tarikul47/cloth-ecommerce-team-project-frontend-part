8b5156cbd08d1971d59b21538c73ad4dbbdd0770
Rakib1415 <rakibiuaece@gmail.com>
2022-11-02 01:14:18 +0600

    feat: mobile menu toggle and header responsive completed

---
+++ public/menuData.js
@@ -0,0 +1,76 @@
+export const menuItems = [
+    {
+        id : 101,
+        name : 'HOME',
+        url : '/'
+    },
+    {
+        id : 102,
+        name : 'MEN',
+        url : ''
+    },
+    {
+        id : 103,
+        name : 'WOMEN',
+        url : ''
+    },
+    {
+        id : 104,
+        name : 'KIDS',
+        url : ''
+    },
+    {
+        id : 105,
+        name : 'CART',
+        url : '/cart'
+    },
+    {
+        id : 106,
+        name : 'CHECKOUT',
+        url : '/checkout'
+    },
+    {
+        id : 107,
+        name : 'THANKYOU',
+        url : '/thankyou'
+    }
+];
+
+export const subMenuItems = [
+    {
+        id : 201,
+        name : 'NEW IN',
+        url : '/category/pants'
+    },
+    {
+        id : 202,
+        name : 'PANJABI',
+        url : '/category/panjabi'
+    },
+    {
+        id : 203,
+        name : 'NEW IN',
+        url : '/category/pants'
+    },
+    {
+        id : 204,
+        name : 'PANTS',
+        url : '/category/pants'
+    },
+    {
+        id : 205,
+        name : 'JACKETS',
+        url : '/category/jackets'
+    },
+    {
+        id : 206,
+        name : 'SHIRTS',
+        url : '/category/shirts'
+    },
+    {
+        id : 207,
+        name : 'SPORTSWEAR',
+        url : '/category/sportswear'
+    }
+
+]
